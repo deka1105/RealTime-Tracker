bed95e62b8061555c9490641f7d9b2b01b683654
Shubham A. D <acefifth@gmail.com>
2024-02-20 19:24:01 -0500

    Update pilot.py

--- pilot.py
+++ pilot.py
@@ -17,11 +17,11 @@ plot = st.pyplot(fig)
 while True:
     # Get the historical prices for Apple stock
     historical_prices = apple_stock.history(period='1d', interval='1m')
-    
+
     # Get the latest price and time
     latest_price = historical_prices['Close'].iloc[-1]
     latest_time = historical_prices.index[-1].strftime('%H:%M:%S')
-    
+
     # Clear the plot and plot the new data
     ax.clear()
     ax.plot(historical_prices.index, historical_prices['Close'], label='Stock Value')
@@ -30,12 +30,12 @@ while True:
     ax.set_title('Apple Stock Value')
     ax.legend(loc='upper left')
     ax.tick_params(axis='x', rotation=45)
-    
+
     # Update the plot in the Streamlit app
     plot.pyplot(fig)
-    
+
     # Show the latest stock value in the app
     st.write(f"Latest Price ({latest_time}): {latest_price}")
-    
+
     # Sleep for 1 minute before fetching new data
-    time.sleep(60)
+    time.sleep(20)
